edf59b6fc36c1e5c16803f9f7db676331185e675
DhairyaBokadia <67190921+DhairyaBokadia@users.noreply.github.com>
2020-07-16 12:41:35 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,20 @@
+class Ball{
+     constructor(){
+        var options={
+          isStatic:false,
+      restitution:0.3,
+          friction:0.5,
+          density:1.2
+        }
+        this.body=Bodies.circle(140,650,20,options);
+        this.radius=20
+
+        World.add(world,this.body)
+     }
+     display(){
+         rectMode(CENTER)
+         var pos=this.body.position
+         circle(pos.x,pos.y,20);
+         fill("green");
+     }
+}
